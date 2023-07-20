e1e64478705ad97dbc08a5bf47a138065d7eb49b
leenar24-meet <139689053+leenar24-meet@users.noreply.github.com>
2023-07-20 17:31:31 +0300

    klvjzj
    dk.c<jz

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ function myFunction() //did this to make sure the files are connected
 	alert("<3")
 }
 function changeBackgroundColor(color){
-	document.body.style.backgroundColor= color;
+	document.body.style.backgroundColor= color;}
 
-}
+
+var bg= object.style.backgroundColor;
+if bg= color
+	
